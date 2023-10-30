3e3370a2b41214da97e59c44e8fdbb7e932e3f3e
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2023-10-30 16:29:54 -0700

    added menu accordingly screen size

--- website/emmanuelnuno/src/app/components/navbar/navbar.component.ts
+++ website/emmanuelnuno/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject, takeUntil } from 'rxjs';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnDestroy{
+  destroyed = new Subject<void>();
+  currentScreenSize: string = "";
+
+  displayNameMap = new Map([
+    [Breakpoints.XSmall, 'XSmall'],
+    [Breakpoints.Small, 'Small'],
+    [Breakpoints.Medium, 'Medium'],
+    [Breakpoints.Large, 'Large'],
+    [Breakpoints.XLarge, 'XLarge']
+  ]);
+
+  constructor(breakpointObserver: BreakpointObserver) {
+    breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge
+      ])
+      .pipe(takeUntil(this.destroyed))
+      .subscribe(result => {
+        for (const query of Object.keys(result.breakpoints)) {
+          if (result.breakpoints[query]) {
+            this.currentScreenSize = this.displayNameMap.get(query) ?? 'Unknown';
+          }
+        }
+      })
+  }
+
+    ngOnDestroy(): void {
+      this.destroyed.next();
+      this.destroyed.complete();
+    }
+
 
 }
