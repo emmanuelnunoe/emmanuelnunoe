7a48b2f16f6044c1de5815d5b3017fbad24e83cf
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2024-02-03 13:19:20 -0800

    added downloadable link!

--- website/emmanuelnuno/src/app/components/aboutme/aboutme.component.ts
+++ website/emmanuelnuno/src/app/components/aboutme/aboutme.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./aboutme.component.css']
 })
 export class AboutmeComponent {
-
+  downloadMyCv()
+  {
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../../assets/Emmanuel Nuño Estrella- Computer Engineer 2024.pdf');
+    link.setAttribute('download', `Emmanuel Nuño Estrella- Computer Engineer 2024.pdf`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
 }
