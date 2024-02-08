ecd65f14814ee30ff950e0a217c29066a6d56632
Emmanuel Nuño <emmanuel.nuno.estrella@uabc.edu.mx>
2024-02-07 16:36:01 -0800

    fixed padding on copy button

--- website/emmanuelnuno/src/app/components/contact/contact.component.ts
+++ website/emmanuelnuno/src/app/components/contact/contact.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent {
+
+  @ViewChild('textToCopy') textToCopy!: ElementRef<HTMLParagraphElement>;
+
+  copyToClipboard() {
+    const email: string = "enunospace@gmail.com";
+    const text: string = (this.textToCopy.nativeElement.textContent?.trim()) == undefined ? "" : this.textToCopy.nativeElement.textContent?.trim();
+    if (text != undefined) {
+      navigator.clipboard.writeText(text);
+
+    }
+  }
+  
 
 }
